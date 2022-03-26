ac452f629a90552a0375c032a864f40535a0c862
NULLPE <1989leosun@gmail.com>
2022-03-26 21:46:53 +0900

    update

---
+++ server/src/test/java/com/example/server/service/impl/UserServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.example.server.service.impl;
+
+import com.example.server.dao.UserDAO;
+import com.example.server.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    @MockBean
+    UserDAO userDAO;
+
+    @Autowired
+    UserServiceImpl it;
+
+    @ParameterizedTest
+    @CsvSource(value = {"test,true","none, false"}, delimiter = ',')
+    void testIsExist(String username, boolean expected) {
+        User user = new User();
+        when(userDAO.findByUsername("test")).thenReturn(user);
+        assertThat(it.isExist(username), is(expected));
+    }
+}
